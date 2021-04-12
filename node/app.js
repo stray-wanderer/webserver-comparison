17c5aac10ece9a837dcb750af4085338f1e417b4
stray-wanderer <trbr1800@student.miun.se>
2021-04-12 19:09:54 +0200

    added imagetest and curloader config

--- node/app.js
+++ node/app.js
@@ -1,14 +1,38 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const os = require('os');
 const port = 5000
+const folder = 'Pics'
+
+function base64encode(file){
+
+	var readFile = fs.readFileSync(file);
+	return new Buffer(readFile).toString('base64');
+}
+
+var files = []
+fs.readdirSync(folder).forEach(file=>{
+	files.push(folder +'/'+ file)
+}) 
+
+var base64Files = []
+for(var i = 0; i < files.length; i++){
+	base64Files.push(base64encode(files[i]))
+}
+
+var html = ''
+for(var i = 0; i < base64Files.length; i++){
+	html += '<img src = "data:image/jpg;base64, ' + base64Files[i] +'"/>'
+}
 
 app.get('/', (req, res) => {
     res.send(`<h3>It's ${os.hostname()}</h3>`);
 })
 
 app.get('/imagetest', (req, res) => {
-    res.send(`imagetest`);
+    res.send(html);
+
 })
 
 app.listen(port, () => {
